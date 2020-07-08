7b285aa7f52fb0ec3ff57dbb7aad70121908455d
Romain <romainaubry85@gmail.com>
2020-07-08 11:30:30 +0200

    ajout level3

--- js/main.js
+++ js/main.js
@@ -133,7 +133,7 @@ Spider.prototype.die = function () {
 
 PlayState = {};
 
-const LEVEL_COUNT = 2;
+const LEVEL_COUNT = 3;
 
 PlayState.init = function (data) {
     this.game.renderer.renderSession.roundPixels = true;
@@ -176,6 +176,7 @@ PlayState.preload = function () {
     // json position
     this.game.load.json('level:0', 'data/level00.json');
     this.game.load.json('level:1', 'data/level01.json');
+    this.game.load.json('level:2', 'data/level02.json');
     // images
     this.game.load.image('background', 'images/Grass.png');
     this.game.load.image('ground', 'images/Sol.png');
